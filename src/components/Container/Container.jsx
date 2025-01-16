255ab0959826217c2fc25e4948e74f48e0aa270b
roman <romanponchka@gmail.com>
2025-01-16 13:29:31 +0200

    basic architecture

---
+++ src/components/Container/Container.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './Container.css'
+
+function Container() {
+
+    return(
+        <div className="container">
+        
+        </div>
+    )
+
+}
+
+export default Container
